6f09105b8df70ecc81e214cf7ae1767d2ec48db7
rumon5 <mdrumonkhan48519@gmail.com>
2022-09-30 20:27:04 +0600

    Appointment component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home';
 import Navbar from './Pages/Shared/Navbar';
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='about' element={<About></About>}></Route>
+        <Route path='appointment' element={<Appointment></Appointment>}></Route>
       </Routes>
     </div>
   );
