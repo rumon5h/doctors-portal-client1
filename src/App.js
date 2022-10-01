1ee78a3c5f4cd34eef654af2eff138be76273f8e
rumon5 <mdrumonkhan48519@gmail.com>
2022-10-01 10:12:51 +0600

    Google signIn added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './Pages/About/About';
 import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home';
+import LogIn from './Pages/LogIn/LogIn';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Home/>}></Route>
         <Route path='about' element={<About></About>}></Route>
         <Route path='appointment' element={<Appointment></Appointment>}></Route>
+        <Route path='login' element={<LogIn></LogIn>}></Route>
       </Routes>
     </div>
   );
--- src/Pages/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const LogIn = () => {
-    return (
-        <div>
-            
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+  if(user){
+    console.log(user);
+  }
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="card max-w-md bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="text-2xl text-center font-bold">LogIn</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="divider">OR</div>
+          <button 
+          onClick={() => signInWithGoogle()}
+          className="btn btn-outline">Continue with Google</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
