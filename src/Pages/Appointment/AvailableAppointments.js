2f7feddbdc8691171fe14a4fb3620e9e1cd7fca1
rumon5h <mdrumonkhan48519@gmail.com>
2023-01-11 00:02:03 +0600

    appointments component

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,30 +1,46 @@
-import { format } from 'date-fns';
-import React, { useEffect, useState } from 'react';
-import BookingModal from './BookingModal';
-import Service from './Service';
+import { format } from "date-fns";
+import React, { useEffect, useState } from "react";
+import BookingModal from "./BookingModal";
+import Service from "./Service";
 
-const AvailableAppointments = ({date}) => {
-    const [services, setServices] = useState([]);
-    const [treatment, setTreatment] = useState(null);
+const AvailableAppointments = ({ date }) => {
+  const [services, setServices] = useState([]);
+  const [treatment, setTreatment] = useState(null);
 
+  const formattedDate = format(date, "PP");
 
-    useEffect(() => {
-      fetch('http://localhost:5000/services')
-      .then(res => res.json())
-      .then(data => setServices(data))
-    }, [])
-    
-    return (
-        <div className='my-14'>
-            <h1 className='text-secondary text-xl text-center'>Available Appointments on {format(date, 'PP')}</h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 m-5'>
-                {
-                    services.map(service => <Service key={service._id} setTreatment={setTreatment} service={service}></Service> )
-                }
-            </div>
-            {treatment && <BookingModal date={date} setTreatment={setTreatment} treatment={treatment}></BookingModal>}
-        </div>
-    );
+  useEffect(() => {
+    fetch(`http://localhost:5000/available?date=${formattedDate}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setServices(data);
+        console.log(data);
+      });
+  }, [formattedDate]);
+
+  return (
+    <div className="my-14">
+      <h1 className="text-secondary text-xl text-center">
+        Available Appointments on {format(date, "PP")}
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 m-5">
+        {services.map((service) => (
+          <Service
+            key={service._id}
+            setTreatment={setTreatment}
+            service={service}
+          ></Service>
+        ))}
+      </div>
+      {treatment && (
+        <BookingModal
+          date={date}
+          setTreatment={setTreatment}
+          treatment={treatment}
+        ></BookingModal>
+      )}
+    </div>
+  );
 };
 
 export default AvailableAppointments;
