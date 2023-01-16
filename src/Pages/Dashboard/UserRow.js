9d4af5ac7ce292564a95a233939fb1df8602de4e
rumon5h <mdrumonkhan48519@gmail.com>
2023-01-16 19:22:50 +0600

    make admin

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -11,10 +11,17 @@ const UserRow = ({user, refetch}) => {
                 authorization: `Bearer ${localStorage.getItem('access_token')}`
             }
         })
-        .then(res => res.json())
+        .then(res => {
+            if(res.status === 403){
+                toast.error('Failed to make an admin')
+            }
+            return res.json()
+        })
         .then(data => {
-            refetch();
+            if(data.modifiedCount > 0){
+                refetch();
             toast.success('Successfully made an admin.')
+            }
         })
     }
   return (
